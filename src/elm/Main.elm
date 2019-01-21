b205bbfb81317e5b39e6f1cd8f2e5070e6ca45cf
jangsa <gute.ist.wieder.tot@gmail.com>
2019-01-22 00:37:08 +0900

    Multiple regex forms with variable length implemented (several minor bugs are not solved)

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,10 @@
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput, targetValue, on)
 import Html.Attributes exposing (..)
 import Regex
---import Debug
+import Json.Decode as JD
+import Debug
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
@@ -13,52 +14,85 @@ main =
 
 type alias Model =
   { domain: String
-  , formNum: Int
-  , index: Int
-  , codomain: String
-  , pattern: String
+  , sections: List Section
   , concatenated: String
+  }
+
+type alias Section =
+  { codomain: String
+  , pattern: String
+  , index: Int
   }
 
 init : Model
 init =
   { domain = ""
-  , formNum = 1
-  , index = 0
-  , codomain = ""
-  , pattern = ""
-  , concatenated = "Concatenated string comes here."
+  , sections = [initSection]
+  , concatenated = initConcatenated
   }
+
+initSection = { codomain = ""
+               , pattern = ""
+               , index = 0
+              }
+initConcatenated = "Concatenated string comes here."
 
 
 -- UPDATE
 
-type Msg = OnDomain String | OnRegex String | OnIndex String
+type Msg = OnDomain String
+         | OnRegex Int String
+         | OnIndex Int String
+         | OnAddBefore Int
+         | OnAddAfter Int
 
+-- TODO: Remove boilerplates in OnRegex & OnIndex
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    OnDomain t -> let match = if String.length model.pattern > 0 then
-                                case getAt model.index <| applyRegex model.pattern t of
-                                  Just s -> s
-                                  Nothing -> ""
-                              else t
-                  in { model | domain = t, codomain = match }
-    OnRegex  r -> let match = if String.length r > 0 then
-                                case getAt model.index <| applyRegex r model.domain of
-                                  Just s -> s
-                                  Nothing -> ""
-                              else model.domain
-                  in { model | pattern = r, codomain = match }
-    OnIndex  i -> let index = case String.toInt i of
-                                Just i_ -> i_
-                                Nothing -> 0
-                      match = if String.length model.pattern > 0 then
-                                case getAt index <| applyRegex model.pattern model.domain of
-                                  Just s -> s
-                                  Nothing -> ""
-                              else model.domain
-                  in { model | index = index, codomain = match }
+  let
+    pivotMatched pattern_ index_ text_ =
+      if String.length pattern_ > 0 then
+        case getAt index_ <| applyRegex pattern_ text_ of
+          Just s -> s
+          Nothing -> ""
+        else text_
+    concatenate strings domain = if String.isEmpty domain then initConcatenated
+                                 else if List.all (\s -> s == domain) strings then domain
+                                 else List.foldr (++) "" <| List.filter (\s -> s /= domain) strings
+    initSection_ = { initSection | codomain = model.domain }
+  in
+    case msg of
+      OnDomain t -> let newSections =
+                          List.map
+                            (\sec ->
+                              let match = pivotMatched sec.pattern sec.index t
+                              in { codomain = match
+                                 , pattern = sec.pattern
+                                 , index = sec.index
+                                 }
+                            )
+                            model.sections
+                    in { model | domain = t, sections = newSections, concatenated = concatenate (List.map (\sec -> sec.codomain) newSections) t }
+      OnRegex n p -> let targetSec = getAt n model.sections
+                     in case targetSec of
+                          Just s -> let match = pivotMatched p s.index model.domain
+                                        newTargetSec = { s | codomain = match, pattern = p }
+                                        newSections = replace n newTargetSec model.sections
+                                    in { model | sections = newSections, concatenated = concatenate (List.map (\sec -> sec.codomain) newSections) model.domain }
+                          Nothing -> model
+                         
+      OnIndex n index -> let targetSec = getAt n model.sections
+                             index_ = case String.toInt index of
+                                  Just i_ -> i_
+                                  Nothing -> 0
+                         in case targetSec of
+                           Just s -> let match = pivotMatched s.pattern index_ model.domain
+                                         newTargetSec = { s | codomain = match, index = index_ }
+                                         newSections = replace n newTargetSec model.sections
+                                     in { model | sections = newSections, concatenated = concatenate (List.map (\sec -> sec.codomain) newSections) model.domain }
+                           Nothing -> model
+      OnAddBefore n -> { model | sections = initSection_ :: model.sections }
+      OnAddAfter n -> { model | sections = model.sections ++ [initSection_] }
 
 getAt : Int -> List a -> Maybe a
 getAt i xs = List.head <| List.drop i xs
@@ -68,9 +102,9 @@ replace : Int -> a -> List a -> List a
 replace target y xs = case xs of
   [] -> []
   (h::t) ->
-    if target < 1 then xs
+    if target < 0 then xs
     else if target > List.length xs then xs
-    else if target == 1 then y :: t
+    else if target == 0 then y :: t
     else h :: (replace (target - 1) y t)
 
 applyRegex : String -> String -> List String
@@ -81,38 +115,52 @@ applyRegex pattern domain =
 by : String -> Regex.Regex
 by pattern = Maybe.withDefault Regex.never <| Regex.fromString pattern
 
+
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div []
     [ div []
-          [ h1 [] [text "Regex & Concatenation"]]
-    , hr [] []
+          [ h1 [] [text "Regex & Concatenation"]
+          , hr [] []
+          ]
     , div []
           [ h2 [] [ text "Original Text" ]
           , textarea [placeholder "before", cols 100, rows 20, onInput OnDomain ] [ text model.domain ]
+          , hr [] []
           ]
-    , hr [] []
     , div []
           [ h2 [] [text "Concatenated Text" ]
           , p [] [text model.concatenated ]
+          , hr [] []
           ]
-    , hr [] []
-    , div []
-          [ div []
-                [ button [] [ text "add before" ]
-                ]
-          , h2 [] [ text <| "Regex Pattern " ++ String.fromInt 1 ]
-          , div []
-                [ input [ placeholder "regex", onInput OnRegex ] [] ]
-          , div []
-                [ input [ placeholder "index (default: 0)", onInput OnIndex ] [] ]
-          ]
-          , div []
-                [ textarea [ placeholder "after", cols 100, rows 20, readonly True ] [ text model.codomain ] ]
-          , div []
-                [ button [] [ text "add after" ]
-                ]
+    , div [] <| matchingSections model
     ]
 
+
+onChange : (String -> msg) -> Html.Attribute msg
+onChange tagger =
+  on "change" (JD.map tagger targetValue)
+
+-- TODO: Remove bugs in "add before"/"add after" button
+matchingSections : Model -> List (Html Msg)
+matchingSections model =
+  List.indexedMap
+    (\n -> \sec ->
+      div []
+          [ --button [ onClick (OnAddBefore n) ] [ text "add before" ]
+          --, h2     [] [ text <| "Regex Pattern " ++ String.fromInt (n + 1) ]
+            h2     [] [ text <| "Regex Pattern " ++ String.fromInt (n + 1) ]
+          , div []
+                [ input [ placeholder "regex", onInput (OnRegex n) ] [] ]
+          , div []
+                [ input [ placeholder "index (default: 0)", onInput (OnIndex n) ] [] ]
+          , div []
+                [ textarea [ placeholder "after", cols 100, rows 20, readonly True ] [ text sec.codomain ] ]
+          , div []
+                [ button [ onClick (OnAddAfter n) ] [ text "add after" ] ]
+          , hr [] []
+          ]
+    )
+    model.sections
